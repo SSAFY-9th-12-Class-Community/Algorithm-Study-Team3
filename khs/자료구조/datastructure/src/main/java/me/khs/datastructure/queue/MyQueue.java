c5d37b57e9579b09d7ffd39b95fd60babc360e4f
khs960616 <anjfkrhdkdle@naver.com>
2023-06-10 02:27:09 +0900

    fix: queue 인터페이스 오류 수정

--- khs/자료구조/datastructure/src/main/java/me/khs/datastructure/queue/MyQueue.java
+++ khs/자료구조/datastructure/src/main/java/me/khs/datastructure/queue/MyQueue.java
@@ -2,7 +2,7 @@ package me.khs.datastructure.queue;
 
 public interface MyQueue<E> {
     boolean enqueue(E e);
-    boolean dequeue(E e);
+    boolean dequeue();
     boolean isEmpty();
     E front();
 }
