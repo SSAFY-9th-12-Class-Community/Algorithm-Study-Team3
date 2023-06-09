096340a6613a768855c4c356b960bc2645109479
khs960616 <anjfkrhdkdle@naver.com>
2023-06-10 01:37:12 +0900

    init: 자료구조 학습을 위한 프로젝트 추가

---
+++ khs/자료구조/datastructure/src/main/java/me/khs/datastructure/DatastructureApplication.java
@@ -0,0 +1,12 @@
+package me.khs.datastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatastructureApplication.class, args);
+	}
+}
---
+++ khs/자료구조/datastructure/src/test/java/me/khs/datastructure/DatastructureApplicationTests.java
@@ -0,0 +1,13 @@
+package me.khs.datastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
